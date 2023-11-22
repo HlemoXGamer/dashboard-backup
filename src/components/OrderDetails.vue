4194a688da820ea17bfa6b3ff65d8d0903d57dd7
HlemoXGamer <HlemoXGamer@Gmail.com>
2023-11-22 16:44:06 +0200

    space between ui fix

--- src/components/OrderDetails.vue
+++ src/components/OrderDetails.vue
@@ -87,7 +87,7 @@ const langIdentifier = computed(() => {
             </VRow>
             <VRow v-if="product.cart_extra_flavor?.length > 0">
               <VCard class="ma-5 ml-0 w-100" v-for="(item, index) in product.cart_extra_flavor" :key="index">
-                <VCardText class="d-flex justify-between align-center">
+                <VCardText class="d-flex justify-space-between align-center">
                   <div>Name: {{ item.name }}</div>
                   <div>Price: {{ item.price }}</div>
                   <div>Quantity: {{ item.quantity }}</div>
